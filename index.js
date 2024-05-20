b020b8a1cf7bcfe857581b727ca9d5a56226b98b
hutt <github@jh0.eu>
2024-05-21 00:38:24 +0200

    added animated svg spinner

--- index.js
+++ index.js
@@ -348,7 +348,7 @@ Drucksache 20/10054";https://bundestag.de/dokumente/textarchiv/2024/kw03-de-zwei
         <h2>Vorhandene Daten</h2>
         <p>Hier sind die mit dieser API abrufbaren Daten inkl. Links zum direkten Download aufgelistet. Aktuell sind Abfragen auf Datensätze ab 2020 begrenzt.</p>
         <ul id="data-list" class="data">
-            <li>Daten werden geladen...</li>
+            <li><svg width="12" height="12" stroke="#000" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><style>.spinner_V8m1{transform-origin:center;animation:spinner_zKoa 2s linear infinite}.spinner_V8m1 circle{stroke-linecap:round;animation:spinner_YpZS 1.5s ease-in-out infinite}@keyframes spinner_zKoa{100%{transform:rotate(360deg)}}@keyframes spinner_YpZS{0%{stroke-dasharray:0 150;stroke-dashoffset:0}47.5%{stroke-dasharray:42 150;stroke-dashoffset:-16}95%,100%{stroke-dasharray:42 150;stroke-dashoffset:-59}}</style><g class="spinner_V8m1"><circle cx="12" cy="12" r="9.5" fill="none" stroke-width="3"></circle></g></svg> Daten werden geladen...</li>
         </ul>
     </section>
 
