6339953e68090fe73e387807d4fd156cf072f296
hutt <github@jh0.eu>
2024-05-22 05:19:26 +0200

    small readability improvements

--- index.js
+++ index.js
@@ -246,7 +246,8 @@ async function serveDocumentation() {
 
     <section id="vorhandene-daten">
     <h2>Vorhandene Daten</h2>
-        <p>Hier sind die mit dieser API abrufbaren Daten inklusive Download-Links für verschiedene Formate aufgelistet. Aktuell sind Abfragen auf Datensätze ab 2015 begrenzt. Die Kalenderwochen können mit einem Klick auf das Jahr aufgeklappt werden.</p>
+        <p>Hier sind die mit dieser API abrufbaren Daten inklusive Download-Links für verschiedene Formate aufgelistet. Aktuell sind Abfragen auf Datensätze ab 2015 begrenzt.</p>
+        <p>Die Kalenderwochen können mit einem Klick auf das Jahr aufgeklappt werden.</p>
         <ul id="data-list" class="data">
             <li><svg width="12" height="12" stroke="#000" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><style>.spinner_V8m1{transform-origin:center;animation:spinner_zKoa 2s linear infinite}.spinner_V8m1 circle{stroke-linecap:round;animation:spinner_YpZS 1.5s ease-in-out infinite}@keyframes spinner_zKoa{100%{transform:rotate(360deg)}}@keyframes spinner_YpZS{0%{stroke-dasharray:0 150;stroke-dashoffset:0}47.5%{stroke-dasharray:42 150;stroke-dashoffset:-16}95%,100%{stroke-dasharray:42 150;stroke-dashoffset:-59}}</style><g class="spinner_V8m1"><circle cx="12" cy="12" r="9.5" fill="none" stroke-width="3"></circle></g></svg> Daten werden geladen...</li>
         </ul>
